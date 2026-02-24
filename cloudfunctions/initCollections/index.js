f6240b20b44180fcfc01fc257ff1c59621a32fc4
Hu Yi <huyi@HudeMacBook-Pro.local>
2026-02-24 22:57:02 +0800

    fix: 修复排行榜无数据和个人速通记录被清除问题；移除无用集合

--- cloudfunctions/initCollections/index.js
+++ cloudfunctions/initCollections/index.js
@@ -2,7 +2,7 @@
 const cloud = require('wx-server-sdk')
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV })
 
-const COLLECTIONS = ['playerData', 'leaderboard', 'rankAll', 'rankDaily', 'rankDex', 'rankCombo']
+const COLLECTIONS = ['playerData', 'rankAll', 'rankDex', 'rankCombo']
 
 exports.main = async (event, context) => {
   const db = cloud.database()
